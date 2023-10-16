3b2b3db48f66c69638925c5e0649b0cb874a0ef1
Baigorria Juan <santibaigo1986@gmail.com>
2023-10-16 16:22:32 -0300

    Despues de los paquetes

--- Models/Data/ApplicationDbContext.cs
+++ Models/Data/ApplicationDbContext.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MaestroDetalleCRUD.Models.Data
 {
     public class ApplicationDbContext:DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>option) : base(option)
+        {
+
+        }  
+        public DbSet<Cliente> Clientes {get;set;} = null!;
+        public DbSet<Producto> Productos {get;set;} = null!;
+        public DbSet<Pedido> Pedidos {get;set;} = null!;
+        public DbSet<PedidoDetalle> PedidoDetalles {get;set;} = null!;
     }
 }
